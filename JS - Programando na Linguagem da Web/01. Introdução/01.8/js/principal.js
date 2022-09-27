59986fc87d4ef50cf409efca2d3695df603c4c34
tthtails <thaslesouza@gmail.com>
2022-09-26 21:06:20 -0300

    Capítulo 1 do curso concluído: introdução

---
+++ JS - Programando na Linguagem da Web/01. Introdução/01.8/js/principal.js
@@ -0,0 +1,15 @@
+			// alert ("Olá mundo!");
+			// console.log ("Olá mundo!"); console na aba "Ferramentas do Programador".
+			// console.log (document);
+			// console.log (document.querySelector("h1"));
+
+			// var title = document.querySelector("h1");
+			// console.log(title); já que tem apenas "title" ele imprime a tag inteira.
+			// console.log(title.textContent); quando imprime com textContent ele mostra o que está dentro da tag.
+
+			// title.textContent = "jorge cria!" - altera o que está na dentro da tag.
+
+			// var title = document.querySelector(".title");
+			// console.log(title);
+		
+            console.log("Fui carregado de um arquivo externo :))");
